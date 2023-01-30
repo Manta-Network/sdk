58654c755b8c7180202aaa64ca8c3609b0c4a6b2
Georgi Zlatarev <georgi.zlatarev@manta.network>
2023-01-30 23:35:33 +0200

    No need for null
    Signed-off-by: Georgi Zlatarev <georgi.zlatarev@manta.network>

--- manta-js/package/src/privateWallet.ts
+++ manta-js/package/src/privateWallet.ts
@@ -560,7 +560,7 @@ export class MantaPrivateWallet implements IMantaPrivateWallet {
 }
 
 // removes first 3 bytes and json bits of sign-result
-async function getXCMRemoteTransactPayload(signResult: any): Promise<string | null> {
+async function getXCMRemoteTransactPayload(signResult: any): Promise<string> {
   let res = JSON.stringify(signResult.txs).slice(10);
   let raw = res.replace("[", "").replace("\"", "").replace("]", "");
   return raw;
